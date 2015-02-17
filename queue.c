b2e2fb72a4e6cf7235ab46de1a50e625eccbe9c3
Killthebug <jaipal.singh@research.iiit.ac.in>
2015-02-17 11:39:06 +0530

    Stack Beta

---
+++ queue.c
@@ -0,0 +1,70 @@
+// #Authored : Jaipal Singh Goud
+// Part of : My STL 
+// Date :
+// Sem I-II
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <malloc.h>
+
+typedef struct xyz{
+    int data;
+    struct xyz *next;
+}node;
+
+node * dequeue(node *head);
+void printlist(node *head);
+
+node *fordel,*head,*tail,*cur,*new;
+int len,x,i,n;
+
+int main()
+{
+    new = (node *)malloc(sizeof(node));                     // Initialized the Very first node (Allocates some memory to it)
+    head = new;
+    printf("Enter Length of linked list ");
+    scanf("%d",&len);
+    printf("Enter List Elements\n");
+    for(i=0;i<len;i++)
+    {
+        scanf("%d",&x);
+        new->data = x;
+        cur = (node *)malloc(sizeof(node));                 // After data is allocated to previous node, a new node is then created
+        new->next = cur;
+        new = cur;
+        new->next = NULL;
+    }
+    printlist(head);
+    printf("How many elements do you want to delete?");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        fordel = dequeue(head);
+        head = fordel;
+    }
+    printlist(head);
+    return 0;
+}
+
+void printlist(node *head)                                  // Prints list
+{
+    node *temp;
+    temp = head;
+    while(temp->next != NULL)
+    {
+        printf("%d -> ",temp->data);
+        temp = temp->next;
+    }
+    printf("END \n");
+}
+
+node * dequeue(node *head)
+{
+    node *temp;
+    temp = head;
+    head = head->next;
+    free(temp);
+    return head;
+}
+
